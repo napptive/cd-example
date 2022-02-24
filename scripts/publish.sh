87baeda115b80a21985b114073c75273e1aa7f40
Carmendelope <carmen@napptive.com>
2022-02-24 13:52:15 +0100

    tees

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,36 +1,36 @@
 #/bin/bash
 
-if [[ -z "${PLAYGROUND_PAT}" ]]; then
-  echo "PLAYGROUND_PAT must be set with your Personal Access Token"
-  return 1
-else
-  echo "PLAYGROUND_PAT is set."
-fi
+#if [[ -z "${PLAYGROUND_PAT}" ]]; then
+#  echo "PLAYGROUND_PAT must be set with your Personal Access Token"
+#  return 1
+#else
+#  echo "PLAYGROUND_PAT is set."
+#fi
 
-echo "Downloading NAPPTIVE Playground CLI"
-curl -O https://storage.googleapis.com/artifacts.playground.napptive.dev/linux/playground/playground_v1.0.0-rc4.tgz
-tar --strip-components=1 -zxf playground_v1.0.0-rc4.tgz
-chmod +x playground
+#echo "Downloading NAPPTIVE Playground CLI"
+#curl -O https://storage.googleapis.com/artifacts.playground.napptive.dev/linux/playground/playground_v1.0.0-rc4.tgz
+#tar --strip-components=1 -zxf playground_v1.0.0-rc4.tgz
+#chmod +x playground
 
-echo "Downloading Kubectl"
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x kubectl
+#echo "Downloading Kubectl"
+#curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+#chmod +x kubectl
 
 echo "Login into NAPPTIVE Playground"
-./playground login --pat --loginURL=https://development.napptive.dev/oauth/login?source=cli --serverAddress=api.development.napptive.dev --debug
+playground login --pat 
 
 echo "Obtaining kube-config"
-./playground get-kubeconfig --loginURL=https://development.napptive.dev/oauth/login?source=cli --serverAddress=api.development.napptive.dev
+playground get-kubeconfig 
 
 echo "Deploying latest versions"
-./playground cluster info --loginURL=https://development.napptive.dev/oauth/login?source=cli --serverAddress=api.development.napptive.dev
-export PATH=$PATH:$(pwd)
+playground cluster info 
+#export PATH=$PATH:$(pwd)
 echo "Checking if application needs to be deployed"
-./kubectl --kubeconfig napptive-kubeconfig get applicationconfigurations.core.oam.dev cd-example
+kubectl --kubeconfig napptive-kubeconfig get applicationconfigurations.core.oam.dev cd-example
 if [ $? -eq 0 ]; then
     echo "Forcing rolling update to download newest image"
-    ./kubectl --kubeconfig napptive-kubeconfig patch deployment cd-example -p "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"CI-updated\":\"`date +'%s'`\"}}}}}"
+    kubectl --kubeconfig napptive-kubeconfig patch deployment cd-example -p "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"CI-updated\":\"`date +'%s'`\"}}}}}"
 else
     echo "Launching application"
-    ./kubectl --kubeconfig napptive-kubeconfig create -f ./build/k8s
+    kubectl --kubeconfig napptive-kubeconfig create -f ./build/k8s
 fi
